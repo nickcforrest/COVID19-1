e640c8627d905e568fa8e00e1293b5f05403c125
treypujats <52941508+treypujats@users.noreply.github.com>
2020-03-29 01:23:32 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -212,62 +212,59 @@ HospitalIncreases<-function(HospitalDataSet, BaseLocationDataSet, ChosenBase, Ra
     StillHospital<-ceiling((TotalHospital-NotHospital))
     Upper<-(signif((StillHospital/TotalBeds*.314+.6)*100,3))
     Lower<-(signif((StillHospital/TotalBeds*.207+.6)*100,3))
-    if ((Upper - Lower) >= 1) {
-        paste0(Lower, Upper, sep = "-")
-    } else  {
-        paste0(formatC(Upper), "%")
-    }
-    
+    paste(Lower, Upper, sep = "-")
 }
 
-
-CovidCasesPerDayChart<-function(CovidCasesDataSet, CovidDeathDataSet, HospitalDataSet, BaseLocationDataSet, ChosenBase, Radius, CountyData){
+#Begin function to create chart of new cases for COVID-19 is a specified region around a specified base
+CovidCasesPerDayChart<-function(ChosenBase, Radius){
     #Finds number of hospitals in radius
-    BaseStats<-dplyr::filter(BaseLocationDataSet, Base == ChosenBase)
+    BaseStats<-dplyr::filter(AFBaseLocations, Base == ChosenBase)
     for (i in 1:7581) {
-        HospitalDataSet$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(HospitalDataSet$LONGITUDE[i], HospitalDataSet$LATITUDE[i]), fun = distHaversine)/1609.34)
+        HospitalInfo$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(HospitalInfo$LONGITUDE[i], HospitalInfo$LATITUDE[i]), fun = distHaversine)/1609.34)
     }
-    IncludedHospitals<-dplyr::filter(HospitalDataSet, (DistanceMiles <= Radius))
+    IncludedHospitals<-dplyr::filter(HospitalInfo, (DistanceMiles <= Radius))
     IncludedHospitals<-dplyr::filter(IncludedHospitals, (TYPE=="GENERAL ACUTE CARE") | (TYPE=="CRITICAL ACCESS"))
     TotalBeds<-sum(IncludedHospitals$BEDS)
     
     #Finds which counties in given radius. Also Give county statistics
-    BaseStats<-dplyr::filter(BaseLocationDataSet, Base == ChosenBase)
+    BaseStats<-dplyr::filter(AFBaseLocations, Base == ChosenBase)
     for (i in 1:3143) {
-        CountyData$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(CountyData$Longitude[i], CountyData$Latitude[i]), fun = distHaversine)/1609.34)
+        CountyInfo$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(CountyInfo$Longitude[i], CountyInfo$Latitude[i]), fun = distHaversine)/1609.34)
     }
-    IncludedCounties<-dplyr::filter(CountyData, DistanceMiles <= Radius)
-    CovidCounties<-subset(CovidCasesDataSet, CountyFIPS %in% IncludedCounties$FIPS)
-    VectDailyCovid<-colSums(CovidCounties)
+    IncludedCounties<-dplyr::filter(CountyInfo, DistanceMiles <= Radius)
+    CovidCounties<-subset(CovidConfirmedCases, CountyFIPS %in% IncludedCounties$FIPS)
+    VectDailyCovid<-colSums(CovidCounties[5:length(CovidCounties)])
     DailyNewCases<-VectDailyCovid[2:length(VectDailyCovid)]-VectDailyCovid[1:(length(VectDailyCovid)-1)]
+    DailyNewCases
     DailyNewHospitalizations<-ceiling(DailyNewCases*.26)
+    DailyNewHospitalizations
     
     
     #Find New Deaths
-    BaseStats<-dplyr::filter(BaseLocationDataSet, Base == ChosenBase)
+    BaseStats<-dplyr::filter(AFBaseLocations, Base == ChosenBase)
     for (i in 1:3143) {
-        CountyData$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(CountyData$Longitude[i], CountyData$Latitude[i]), fun = distHaversine)/1609.34)
+        CountyInfo$DistanceMiles[i]<-(distm(c(BaseStats$Long, BaseStats$Lat), c(CountyInfo$Longitude[i], CountyInfo$Latitude[i]), fun = distHaversine)/1609.34)
     }
-    IncludedCounties<-dplyr::filter(CountyData, DistanceMiles <= Radius)
-    CovidCountiesDeath<-subset(CovidDeathsDataSet, CountyFIPS %in% IncludedCounties$FIPS)
+    IncludedCounties<-dplyr::filter(CountyInfo, DistanceMiles <= Radius)
+    CovidCountiesDeath<-subset(CovidDeaths, CountyFIPS %in% IncludedCounties$FIPS)
     VectDailyDeaths<-colSums(CovidCountiesDeath[5:ncol(CovidCountiesDeath)])
     DailyNewDeaths<-VectDailyDeaths[2:length(VectDailyDeaths)]-VectDailyDeaths[1:(length(VectDailyDeaths)-1)]
     
     
-    #ForecastDate<- as.numeric(seq(as.Date("2020-01-22"), length=(length(DailyNewDeaths)), by="1 day"))
-    ForecastDate<-seq(from=1, to=length(DailyNewCases), by =1)
+    ForecastDate<- seq(as.Date("2020-01-22"), length=(length(DailyNewDeaths)), by="1 day")
     Chart1Data<-cbind.data.frame(ForecastDate,DailyNewCases,DailyNewHospitalizations,DailyNewDeaths)
     colnames(Chart1Data)<-c("ForecastDate","New Cases","New Hospitalizations","New Deaths")
     Chart1DataSub <- melt(data.table(Chart1Data), id=c("ForecastDate"))
     
     #Plot the forecasts from above but include the actual values from the test data to compare accuracy.
-    ggplot(Chart1DataSub) + geom_line(aes(x=as.numeric(ForecastDate), y=value, colour = variable), size = 1) +
+    ggplot(Chart1DataSub) + geom_line(aes(x=ForecastDate, y=value, colour = variable), size = 1) +
         scale_colour_manual(values=c("Blue", "Orange", "Red"))+
         xlab('Date') +
         ylab('Number of People') +
         ggtitle("COVID-19 Impact", subtitle = "Local Area") +
         theme(text = element_text(size = 15)) +
         labs(color='Legend')
+    
     
     
     
@@ -317,7 +314,7 @@ server <- function(input, output) {
     
     #Create first plot of local health population 
     output$LocalHealthPlot1<-renderPlot({
-        CovidCasesPerDayChart(CovidConfirmedCases, CovidDeaths, HospitalInfo, AFBaseLocations, input$Base, input$Radius, CountyInfo)
+        CovidCasesPerDayChart(input$Base, input$Radius)
     })
     
 
